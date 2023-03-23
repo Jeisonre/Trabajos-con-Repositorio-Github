82a93e93db3add050cb272db836310160d19acc2
Jeisonre <jeisonespin25@gmail.com>
2023-03-22 19:38:16 -0500

    config

---
+++ Card Perfil/main.js
@@ -0,0 +1,26 @@
+let boton = document.querySelector(".profile-card .orange");
+let contador = document.querySelector(".profile-card .contador");
+
+let followers = 263;
+
+boton.addEventListener("click", function() {
+    if (boton.textContent === "Seguir") {
+        followers++;
+        boton.textContent = "Siguiendo";
+    } else {
+        followers--;
+        boton.textContent = "Seguir";
+    }
+
+    contador.textContent = `${followers}`;
+});
+
+const sendEmail = () => {
+    const name = document.querySelector('h1').textContent;
+    const email = 'leandroespin2002@gmail.com';
+    const subject = `Mensaje de ${name}`;
+    const body = `Hola, ${name}.\n\ndeseo contactarme contigo.`;
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+}
+
+document.querySelector('#send-message-btn').addEventListener('click', sendEmail);
